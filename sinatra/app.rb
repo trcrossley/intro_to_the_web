4dedde19d54a2e0fae7485f27b6bbcd6990b8fa9
Tristan Crossley <trcrossley@gmail.com>
2015-12-07 17:50:46 +0000

    Added cat_form with post method

--- sinatra/app.rb
+++ sinatra/app.rb
@@ -17,7 +17,11 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+get '/cat-form' do
+  erb(:cat_form)
+end
+
+post '/named-cat' do
   p params
   @name = params[:firstname]
   @last_name = params[:surname]
